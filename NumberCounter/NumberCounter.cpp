68297758d18a9ce9ac24c7453f2839bb0cc35019
sonicxspeed64 <kimdavid98.korieteh@gmail.com>
2024-10-24 20:55:27 -0400

    Press up or down to increment.

--- NumberCounter/NumberCounter.cpp
+++ NumberCounter/NumberCounter.cpp
@@ -1,20 +1,47 @@
-// NumberCounter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// NumberCounter.cpp : Use back shoulder buttons to increment or decrement counter.
 //
 
 #include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <Windows.h>
+
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int counter = 0;
+    string textFile = "NUMBER.txt";
+
+    ifstream input(textFile);
+    if (input)
+    {
+        input >> counter;
+        input.close();
+    }
+
+    cout << "Press up to increment, or down to decrement." << endl;
+
+    while (true)
+    {
+        int prevInput = counter;
+
+        int key = _getch();
+        if (key == 72) counter++;
+        if (key == 80) counter--;
+
+        if (prevInput != counter)
+        {
+            cout << "Counter: " << counter << endl;
+
+            ofstream output(textFile);
+            output << counter;
+            output.close();
+        }
+    }
+
+    
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
